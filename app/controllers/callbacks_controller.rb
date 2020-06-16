deac0632417b761aed8e8206cd112af4d1d8eccf
javante_nelson <javantenelson@gmail.com>
2020-06-16 18:22:17 -0500

    authorize users to sign in through Github

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,8 @@
-class CallbacksController < Devise::OmniauthCallbacks
+class CallbacksController < Devise::OmniauthCallbacksController
     def github
         binding.pry
-        
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+
+        sign_in_and_redirect @user
     end 
 end
