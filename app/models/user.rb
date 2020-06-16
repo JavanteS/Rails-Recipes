84f649b3cae9f1debc0db1c8f4afb876a2ae6726
javante_nelson <javantenelson@gmail.com>
2020-06-16 18:43:12 -0500

    add category model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable
 
          def self.from_omniauth(auth)
-          where(provider: auth.provider, uid: auth.uid).first_create do |user|
+          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
             user.provider = auth.provider
             user.username = auth.info.name
             user.id = auth.id
