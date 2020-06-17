bce23de11adc4ccf3b3d162362ce42f991dced85
javante_nelson <javantenelson@gmail.com>
2020-06-17 08:22:34 -0500

    add nested routes and routes to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,30 @@
 class RecipesController < ApplicationController
+
+    def index
+        @recipes = Recipe.all
+    end
+
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        @recipe = Recipe.new()
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:, :last_name, :email, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  root to: "application#welcome"
   resources :recipes
   resources :categories
+
+  resources :categories do
+    resources :recipes, only: [:index, :show, :new]
+  end
+
+
   devise_for :users, :controllers => {:registrations => 'registrations', :omniauth_callbacks => "callbacks" }
 
   devise_scope :user do 
     get "/login", to: "devise/sessions#new"
     get "/signup", to: "devise/registrations#new"
   end
-  root to: "application#welcome"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
