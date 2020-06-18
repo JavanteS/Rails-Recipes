9dde1049c85e08539a64717ee1582027d938f5fc
javante_nelson <javantenelson@gmail.com>
2020-06-17 19:17:14 -0500

    add style to the application

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :recipes
   has_many :users, through: :recipes
+  accepts_nested_attributes_for :recipes
   validates :name, presence: true
   validates :name, uniqueness: true
 end
