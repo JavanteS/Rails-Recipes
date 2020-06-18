55fbb7460c7048fb975d986caa4b4287c6476533
javante_nelson <javantenelson@gmail.com>
2020-06-17 20:18:08 -0500

    format nav bar to top of screen

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,24 @@
 class CategoriesController < ApplicationController
+
+
+    def index
+        @categories = Category.all
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+
 end
